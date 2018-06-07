95b3e615ba2b0d618066a6b06fb025e59689b3f1
Allan Costa <allaninocencio@yahoo.com.br>
2018-06-07 14:37:39 -0700

    Return time object in business_days_from_utc_time method. Fix #8.

--- spec/business_days_from_utc_time_spec.rb
+++ spec/business_days_from_utc_time_spec.rb
@@ -8,17 +8,17 @@ describe 'test' do
     # This date, in Brasilia time, will be 2018-03-02 (-02:00)
     time = Time.parse('2018-01-02T02:50:00Z')
 
-    expect(BusinessDays.business_days_from_utc_time(1, time, reference)).to eql(Date.parse("2018-01-03"))
+    expect(BusinessDays.business_days_from_utc_time(1, time, reference)).to eql(Time.parse('2018-01-03T02:00:00Z'))
   end
 
-  it '2018-03-01 plus 1 business days should be 2018-02-02' do
+  it '2018-03-01 plus 1 business days should be 2018-03-02' do
     # Reference data is not in DST (-03:00)
     reference = Time.parse('2018-03-02T02:50:00Z')
 
     # This date, in Brasilia time, will be 2018-03-01 (-03:00)
     time = Time.parse('2018-03-02T02:50:00Z')
 
-    expect(BusinessDays.business_days_from_utc_time(1, time, reference)).to eql(Date.parse("2018-03-02"))
+    expect(BusinessDays.business_days_from_utc_time(1, time, reference)).to eql(Time.parse('2018-03-02T03:00:00Z'))
   end
 
   it '2018-01-04 plus 5 business days should be 2018-01-11' do
@@ -28,7 +28,7 @@ describe 'test' do
     # This date, in Brasilia time, will be 2018-01-04 (-02:00)
     time = Time.parse('2018-01-04T15:00:00Z')
 
-    expect(BusinessDays.business_days_from_utc_time(5, time, reference)).to eql(Date.parse("2018-01-11"))
+    expect(BusinessDays.business_days_from_utc_time(5, time, reference)).to eql(Time.parse('2018-01-11T02:00:00Z'))
   end
 
   it '2018-04-30 plus 2 business days should be 2018-05-03' do
@@ -38,7 +38,7 @@ describe 'test' do
     # This date, in Brasilia time, will be 2018-04-30 (-03:00)
     time = Time.parse('2018-04-30T15:00:00Z')
 
-    expect(BusinessDays.business_days_from_utc_time(2, time, reference)).to eql(Date.parse("2018-05-03"))
+    expect(BusinessDays.business_days_from_utc_time(2, time, reference)).to eql(Time.parse('2018-05-03T03:00:00Z'))
   end
 
   it '2018-04-26 plus 10 business days should be 2018-05-11' do
@@ -47,7 +47,7 @@ describe 'test' do
     # This date, in Brasilia time, will be 2018-04-26 (-03:00)
     time = Time.parse('2018-04-26T15:00:00Z')
 
-    expect(BusinessDays.business_days_from_utc_time(10, time)).to eql(Date.parse("2018-05-11"))
+    expect(BusinessDays.business_days_from_utc_time(10, time)).to eql(Time.parse('2018-05-11T03:00:00Z'))
   end
 
   it '2018-04-26 plus 10 business days should be 2018-05-11' do
@@ -57,6 +57,6 @@ describe 'test' do
     # This date, in Brasilia time, will be 2018-04-26 (-02:00)
     time = Time.parse('2018-04-26T02:50:00Z')
 
-    expect(BusinessDays.business_days_from_utc_time(10, time, reference)).to eql(Date.parse("2018-05-11"))
+    expect(BusinessDays.business_days_from_utc_time(10, time, reference)).to eql(Time.parse('2018-05-11T02:00:00Z'))
   end
 end
