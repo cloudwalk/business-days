8b19ef34a28dacbcd2feacc2a554202f078d1cd3
Allan Costa <allan@cloudwalk.io>
2021-01-04 13:28:39 -0300

    Fix some tests to work with latest holidays list

--- spec/business_days_from_utc_time_spec.rb
+++ spec/business_days_from_utc_time_spec.rb
@@ -22,18 +22,18 @@ describe 'test' do
     expect(BusinessDays.business_days_from_utc_time(5, time)).to eql(Time.parse('2018-01-11T02:00:00Z'))
   end
 
-  it '2018-04-30 plus 2 business days should be 2018-05-03' do
-    # This date, in Brasilia time, will be 2018-04-30 (-03:00)
-    time = Time.parse('2018-04-30T15:00:00Z')
+  it '2021-04-30 plus 2 business days should be 2021-05-04' do
+    # This date, in Brasilia time, will be 2021-04-30 (-03:00)
+    time = Time.parse('2021-04-30T15:00:00Z')
 
-    expect(BusinessDays.business_days_from_utc_time(2, time)).to eql(Time.parse('2018-05-03T03:00:00Z'))
+    expect(BusinessDays.business_days_from_utc_time(2, time)).to eql(Time.parse('2021-05-04T03:00:00Z'))
   end
 
-  it '2018-04-26 plus 10 business days should be 2018-05-11' do
-    # This date, in Brasilia time, will be 2018-04-26 (-03:00)
-    time = Time.parse('2018-04-26T15:00:00Z')
+  it '2021-04-26 plus 10 business days should be 2021-05-11' do
+    # This date, in Brasilia time, will be 2021-04-26 (-03:00)
+    time = Time.parse('2021-04-26T15:00:00Z')
 
-    expect(BusinessDays.business_days_from_utc_time(10, time)).to eql(Time.parse('2018-05-11T03:00:00Z'))
+    expect(BusinessDays.business_days_from_utc_time(10, time)).to eql(Time.parse('2021-05-10T03:00:00Z'))
   end
 
   it '2018-11-02 plus 1 business days should be 2018-11-05' do
